f55cf77668d35cf8a9c4bf48de26a37c20755598
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-08-16 19:32:17 +0000

    Added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
-from products.model import Product
+from products.models import Product
 
 # Create your views here.
 
