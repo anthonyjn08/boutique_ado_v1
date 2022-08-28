4b203c6687b04c390f15436bc0862772313f9bb8
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-08-28 17:45:34 +0000

    Added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -27,14 +27,14 @@ class Order(models.Model):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid64().hex.upper()
+        return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
-        Update grand total each time a line item is aded,
+        Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -44,7 +44,7 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Overeride the original save method to set the order number
+        Override the original save method to set the order number
         if it hasn't been set already.
         """
         if not self.order_number:
@@ -64,8 +64,8 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Overeride the original save method to set the order number
-        if it hasn't been set already.
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
