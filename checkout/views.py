d62a5728fc56acf5039b8bb2cf2b8c1ccdea50f7
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-08-28 12:04:25 +0000

    Added basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,7 @@ from bag.contexts import bag_contents
 
 import stripe
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
