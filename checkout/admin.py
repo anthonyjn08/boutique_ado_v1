9b5f95a2d9f0031db539cd1e1d7e9eefc7cc0c62
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-09-06 00:03:36 +0000

    Completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,28 +2,29 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderLineItemAdminInLine(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInLine,)
+    inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                        'delivery_cost', 'order_total',
-                        'grand_total',)
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
-            'email', 'phone_number', 'country',
-            'postcode', 'town_or_city','street_address1',
-            'street_address2', 'county', 'delivery_cost',
-            'order_total', 'grand_total')
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total')
-
+                    'grand_total',)
     ordering = ('-date',)
 
 admin.site.register(Order, OrderAdmin)
